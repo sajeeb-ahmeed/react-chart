63100d2de4708ae8c7913c8a2a307294cda5d169
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-03-30 19:34:05 +0600

    axois , chartjs , react animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Myline from './Components/Myline/Myline';
+import { useSpring, animated } from 'react-spring'
+import { useState } from 'react';
 
 function App() {
+  const [flip, set] = useState(false)
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    // config: config.molasses,
+    onRest: () => set(!flip),
+
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ fontSize: '40px' }}>
+
+        <animated.div style={props}>I will fade in</animated.div>
+      </div>
+      <Myline></Myline>
     </div>
   );
 }
